f496583e52903ee69efaa19b302d6780b02444c6
Alejandro Rivello <alejandrivello@gmail.com>
2023-08-16 09:40:35 -0700

    task 0 & README

---
+++ web_flask/0-hello_route.py
@@ -0,0 +1,13 @@
+#!/user/bin/python3
+"""Starts a new web app"""
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/", strict_slashes=False)
+def hello_route():
+    """A welcome page"""
+    return "Hello HBNB!"
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
