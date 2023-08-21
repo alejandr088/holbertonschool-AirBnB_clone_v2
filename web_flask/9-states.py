746be7058d48a46263e92a69b6364adb05175d1c
alejandr088 <alejandrivello@gmail.com>
2023-08-21 12:07:04 -0700

    task 10

---
+++ web_flask/9-states.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+"""Starts a new web app"""
+from flask import Flask, render_template
+from models import storage
+from models.state import State
+
+app = Flask(__name__)
+
+
+@app.teardown_appcontext
+def teardown_db(exception):
+    """Close SQLAlchemy session"""
+    storage.close()
+
+
+@app.route('/states', strict_slashes=False)
+@app.route('/states/<id>', strict_slashes=False)
+def states_by_id(id):
+    """States sorted by id"""
+    states = storage.all(State)
+    cons_state = {}
+    obj_id = 0
+    for k, v in states.items():
+        obj_id = k.split('.')
+        if obj_id[1] == id:
+            cons_state = {k: v}
+    return render_template('9-states.html', states=cons_state, id=id)
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
