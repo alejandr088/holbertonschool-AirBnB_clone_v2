91460d3cdd68a48d4648af489cb325b570edd039
alejandr088 <alejandrivello@gmail.com>
2023-08-21 08:28:01 -0700

    task 8

---
+++ web_flask/7-states_list.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""Starts a new web app"""
+from flask import Flask, render_template
+from models import storage
+
+app = Flask(__name__)
+
+
+@app.route('/states_list', strict_slashes=False)
+def states_list():
+    """Display a list of states."""
+    states = storage.all("State")
+    sorted_states = sorted(states.values(), key=lambda state: state.name)
+    return render_template('7-states_list.html', states=sorted_states)
+
+
+@app.teardown_appcontext
+def teardown_db(exception):
+    """Close SQLAlchemy session"""
+    storage.close()
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
