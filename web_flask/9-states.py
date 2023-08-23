799bd35ff821529cf9e9b85fbfa3f2c1d0cf9c51
Alejandro Rivello <alejandrivello@gmail.com>
2023-08-23 10:18:35 -0700

    try task 9

--- web_flask/9-states.py
+++ web_flask/9-states.py
@@ -3,20 +3,25 @@
 from flask import Flask, render_template
 from models import storage
 from models.state import State
-storage.all()
 
 app = Flask(__name__)
 
 
 @app.route('/states', strict_slashes=False)
+def states_list():
+    """List of states"""
+    states = storage.all(State)
+    return render_template('8-cities_by_states.html', states=states)
+
+
 @app.route('/states/<id>', strict_slashes=False)
 def states_by_id(id=None):
     """States sorted by id"""
     states = storage.all(State)
     cons_state = []
     for k, v in states.items():
-        if k.split[1] == id:
-            cons_state.append(v)
+        if k.split('.')[1] == id:
+            cons_state = {k: v}
     return render_template('9-states.html', states=cons_state, id=id)
 
 
